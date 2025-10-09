a0b3e9f0f4652e8b78645365e6060c331b414694
Chengyi-J <252608004@mchk.edu.hk>
2025-10-09 15:59:00 +0800

    Button

---
+++ Index.js
@@ -0,0 +1,11 @@
+function buttonWasClicked()  {
+    console.log("Clicked!");
+    let a = "Yes, I have been clicked";
+    console.log(a);
+    let b=1;
+    console.log(b);
+    b += 1;
+    b *= 2;
+    console.log(b);
+    console.clear()
+}
