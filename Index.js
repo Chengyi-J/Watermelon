f02a12eba7c03caa616f2a02677a938756851c3d
Chengyi-J <252608004@mchk.edu.hk>
2025-10-23 16:01:49 +0800

    added a random function

--- Index.js
+++ Index.js
@@ -9,6 +9,38 @@ function buttonWasClicked()  {
     console.log(b);
 }
 
+function foo() {
+    let number = Math.floor(Math.random()* 10) ; // 0-10
+    if(number == 0){
+        console.log("amazing")
+    } else if(number == 1){
+        console.log("fabulous")
+    } else if(number == 2){
+        console.log("excellent")
+    } else if(number == 3){
+        console.log("fantastic")
+    } else if(number == 4){
+        console.log("wonderful")
+    } else {
+        console.log("Goodbye")
+    }
+    switch(number){
+        case 5:
+            alert("awesome")
+            break;
+        case 6:
+            alert("great")
+            break;
+        case 7:
+            alert("perfect")
+            break;
+        case 8:
+            alert("unbelievable")
+            break;
+
+    }
+}
+
 function bob() { console.log("asdasd") }
 function timer() {
     let count = 0;
